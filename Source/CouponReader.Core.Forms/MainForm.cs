9c5f767a1f9058d2d8f4a1b913a6d95288d0c055
carlosfernandescampos-ux <carlos.fernandes.campos@escola.pr.gov.br>
2025-08-26 12:20:56 -0300

    Update MainForm.cs

--- Source/CouponReader.Core.Forms/MainForm.cs
+++ Source/CouponReader.Core.Forms/MainForm.cs
@@ -1,68 +1,20 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Forms;
-using CouponReader.Common.Services;
-using CouponReader.Common.ViewModels;
-
-namespace CouponReader
+using System.Data.Sqlclient
+namespace CadastroClientesApp
 {
-    public partial class MainForm : Form
+    public partial class Mainform : Form
     {
-        public MainForm()
+        public Mainform 
         {
-            InitializeComponent();
+Initializecomponet();
+    ]
+        
+private void btnSalvar_Click(object sender,) EventArgs e)
 
-            InitButtons();
+    {SalvarDadosCliente(txtNome.Text,txtEnderco.Text,txtTelefone.Text,txtEmail.Text)
+        
+            
 
-            ShowCouponDetails(false);
-        }
-
-        private void InitButtons()
-        {
-            scanButton.Click += new EventHandler(OnScanButtonClick);
-            clearButton.Click += new EventHandler(OnClearButtonClick);
-        }
-
-        private void OnScanButtonClick(object sender, EventArgs e)
-        {
-            if (string.IsNullOrEmpty(txtInputCode.Text))
-            {
-                return;
-            }
-
-            var coupon = CouponsService.Instance.FindCouponByCode(txtInputCode.Text);
-
-            if (coupon == null)
-            {
-                return;
-            }
-
-            couponDetails.Init();
-            couponDetails.DataContext = new CouponViewModel(coupon);
-
-            ShowCouponDetails(true);
-        }
-
-        private void OnClearButtonClick(object sender, EventArgs e)
-        {
-            txtInputCode.Text = string.Empty;
-
-            ShowCouponDetails(false);
-        }
-
-        private void OnHistoryButtonClick(object sender, EventArgs e)
-        {
-            couponsHistory.Visible = true;
-            couponsHistoryControl.Visibility = Visibility.Visible;
-        }
-
-        private void ShowCouponDetails(bool show)
-        {
-            couponsHistoryControl.DataContext = new CouponViewModel(null);
-            couponsHistoryControl.Visibility = Visibility.Collapsed;
-            couponsHistory.Visible = false;
-
-            couponDetails.Visibility = show ? Visibility.Visible : Visibility.Collapsed;
-        }
-    }
-}
+        
